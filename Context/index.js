b258cf0bf0d769008872b2a11e284e13fcdac0c2
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-08-02 21:53:00 +0100

    Address

--- Context/index.js
+++ Context/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, createContext, useContext } from 'react';
+import React, { useState, useEffect, createContext, useContext } from 'react';
 import { ethers } from 'ethers';
 import {
 	checkIfWalletConnected,
