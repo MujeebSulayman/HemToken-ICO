aa670072f38762f993a823924ef8bd511084e0c2
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-08-07 15:11:47 +0100

    Add components and routing them

---
+++ components/index.js
@@ -0,0 +1,35 @@
+import About from './About';
+import ArrowUp from './ArrowUp';
+import Banner from './Banner';
+import Blog from './Blog';
+import Client from './Client';
+import Contact from './Contact';
+import Distribution from './Distribution';
+import Faq from './Faq';
+import Header from './Header';
+import Footer from './Footer';
+import Loader from './Loader';
+import MobileApp from './MobileApp';
+import Service from './Service';
+import TimeLine from './TimeLine';
+import Team from './Team';
+import TokenSale from './TokenSale';
+
+export {
+	About,
+	ArrowUp,
+	Banner,
+	Blog,
+	Client,
+	Contact,
+	Distribution,
+	Faq,
+	Header,
+	Footer,
+	Loader,
+	MobileApp,
+	Service,
+	TimeLine,
+	Team,
+	TokenSale,
+};
--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,21 @@ export default function App({ Component, pageProps }) {
 			<StateContextProvider>
 				<Component {...pageProps} />
 			</StateContextProvider>
+
+			<script src='assets/js/jquery-1.12.4.min.js'></script>
+			<script src='assets/js/jquery.countdown.min.js'></script>
+			<script src='assets/js/jquery.counterup.min.js'></script>
+			<script src='assets/js/jquery.dd.min.js'></script>
+			<script src='assets/js/magnific-popup.min.js'></script>
+			{/* <script src='assets/js/notification.js'></script> */}
+			<script src='assets/js/parallax.js'></script>
+			<script src='assets/particles.min.js'></script>
+			<script src='assets/scripts.js'></script>
+			<script src='assets/spop.min.js'></script>
+			<script src='assets/waypoints.min.js'></script>
+			<script src='assets/owlcarousel/js/owl.carousel.min.js'></script>
+			<script src='assets/bootstrap/js/bootstrap.min.js'></script>
+			<script src='index.js'></script>
 		</>
 	);
 }
