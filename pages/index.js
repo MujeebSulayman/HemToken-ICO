9b5d6a5813f9ea7b3805f713e845bfe88db96a61
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-08-07 16:18:46 +0100

    God Abeg o

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,56 @@
 import { useStateContext } from '../context/index';
+import {
+	About,
+	ArrowUp,
+	Banner,
+	Blog,
+	Client,
+	Contact,
+	Distribution,
+	Faq,
+	Header,
+	Footer,
+	Loader,
+	MobileApp,
+	Service,
+	TimeLine,
+	Team,
+	TokenSale,
+} from '../components/index';
 
 const index = () => {
-	const { TOKEN_ICO, transferNativeToken } = useStateContext();
+	const {
+		TOKEN_ICO,
+		transferNativeToken,
+		currentHolder,
+		tokenSale,
+		tokenHolders,
+		nativeToken,
+		balance,
+		address,
+		buyToken,
+		connectWallet,
+		setAddress,
+	} = useStateContext();
 
 	return (
-
-		<div>
-			<div>{TOKEN_ICO}</div>
-			<button onClick={() => transferNativeToken()}> Transfer </button>
+		<div className='v_dark '>
+			<Header
+				address={address}
+				setAddress={setAddress}
+				connectWallet={connectWallet}
+			/>
+			<Banner transferNativeToken={transferNativeToken} />
+			<TokenSale
+				buyToken={buyToken}
+				tokenSale={tokenSale}
+			/>
+			<Distribution />
+			<MobileApp />
+			<Team />
+			<Faq />
+			<Contact />
+			<Footer />
 		</div>
 	);
 };
