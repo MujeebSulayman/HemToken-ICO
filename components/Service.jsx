27cb04824e41cffd5584733337a794c5c1b995cf
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-08-13 12:07:29 +0100

    services

--- components/Service.jsx
+++ components/Service.jsx
@@ -5,32 +5,37 @@ const Service = () => {
 		{
 			title: 'Secure Storage',
 			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis. Fuga sapiente eveniet velit sunt maiores similique minus',
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis.',
 		},
 		{
 			title: 'Mobile App',
 			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis. Fuga sapiente eveniet velit sunt maiores similique minus',
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis./',
 		},
 		{
 			title: 'Planning',
 			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis. Fuga sapiente eveniet velit sunt maiores similique minus',
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis.',
 		},
 		{
 			title: 'Exchange Service',
 			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis. Fuga sapiente eveniet velit sunt maiores similique minus',
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis.',
 		},
 		{
 			title: 'Investment Project',
 			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis. Fuga sapiente eveniet velit sunt maiores similique minus',
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis.' ,
+		},
+		{
+			title: 'Investment Project',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus omnis quasi eum aut est atque, consequatur error quisquam beatae aliquam, mollitia veritatis.',
 		},
 	];
 	return (
 		<section
-			id='section'
+			id='service'
 			className='small_pb'>
 			<div className='container'>
 				<div className='row align-items-center'>
@@ -46,13 +51,28 @@ const Service = () => {
 								className='animation'
 								data-animation='fadeInUp'
 								data-animation-delay='0.2s'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam hic suscipit incidunt, nobis sapiente debitis officiis.
-                </p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+								hic suscipit incidunt, nobis sapiente debitis officiis.
+							</p>
 						</div>
 					</div>
 				</div>
 
-        <div className='row'></div>
+				<div className='row'>
+					{services.map((service, i) => (
+						<div
+							key={i + 1}
+							className='col-lg-4 col-md-6 col-sm-12'>
+							<div
+								className='box_wrap text-center animation'
+								data-animation='fadeInUp'
+								data-animation-delay={`0.${i + 1}s`}>
+								<h4>{service.title}</h4>
+								<p>{service.description}</p>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</section>
 	);
